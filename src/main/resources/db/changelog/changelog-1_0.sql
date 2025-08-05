e9439a6ed3a7d9c9f935a1d942ea68dea1b2b990
phankandy <whota0@163.com>
2025-08-05 22:28:17 +0800

    build(liquibase): 初始化 Liquibase 数据库迁移
    - 添加 Prettier 配置文件以支持 SQL 代码格式化
    - 更新 application-dev.yml 和 application.yml 文件以配置 Liquibase
    - 创建 changelog-1_0.sql 文件并添加初始数据库迁移脚本
    - 优化 SQL 语句格式和结构

--- src/main/resources/db/changelog/changelog-1_0.sql
+++ src/main/resources/db/changelog/changelog-1_0.sql
@@ -1,14 +1,16 @@
 --liquibase formatted sql
--- 
+
 --changeset andy:1 context:prod labels:user 
 --comment: example comment
 CREATE TABLE students (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
     id_number INTEGER NOT NULL,
     age INTEGER NOT NULL,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    PRIMARY KEY (id)
 );
+-- rollback DROP TABLE students;
 
 --changeset andy:2 context:dev labels:user
-ALTER TABLE students
-ADD COLUMN email TEXT;
+ALTER TABLE students ADD COLUMN email TEXT;
+ALTER TABLE students ADD COLUMN school TEXT;
